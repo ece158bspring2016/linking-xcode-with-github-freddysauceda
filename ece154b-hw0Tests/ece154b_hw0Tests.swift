f0679fe08828a69ec0272a0b9e99f33805679bd5
freddysauceda <freddysauceda@gmail.com>
2016-04-01 19:16:54 -0700

    Initial Commit

---
+++ ece154b-hw0Tests/ece154b_hw0Tests.swift
@@ -0,0 +1,36 @@
+//
+//  ece154b_hw0Tests.swift
+//  ece154b-hw0Tests
+//
+//  Created by Freddy Sauceda on 4/1/16.
+//  Copyright © 2016 Alfredo Sauceda. All rights reserved.
+//
+
+import XCTest
+@testable import ece154b_hw0
+
+class ece154b_hw0Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
